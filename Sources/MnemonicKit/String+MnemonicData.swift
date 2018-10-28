786c88135dfcf1e47a3429b2a8b27d743755695e
keefertaylor <keefer.t@gmail.com>
2018-10-28 19:38:14 +0000

    Replacing deprecated string iteration API

--- Sources/MnemonicKit/String+MnemonicData.swift
+++ Sources/MnemonicKit/String+MnemonicData.swift
@@ -2,20 +2,20 @@ import Foundation
 
 public extension String {
 	public func mnemonicData() -> Data {
-		let length = characters.count
+		let length = self.count
 		let dataLength = length / 2
 		var dataToReturn = Data(capacity: dataLength)
 
-		var index = 0
-		var chars = ""
-		for char in characters {
-			chars += String(char)
-			if index % 2 == 1 {
-				let i: UInt8 = UInt8(strtoul(chars, nil, 16))
+    var outIndex = 0
+		var outChars = ""
+    for (_, char) in enumerated() {
+			outChars += String(char)
+			if outIndex % 2 == 1 {
+				let i: UInt8 = UInt8(strtoul(outChars, nil, 16))
 				dataToReturn.append(i)
-				chars = ""
+				outChars = ""
 			}
-			index += 1
+			outIndex += 1
 		}
 
 		return dataToReturn
