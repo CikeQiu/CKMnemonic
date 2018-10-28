342baee371572fa4c2d113a7a03783c1dc6983ed
keefertaylor <keefer.t@gmail.com>
2018-10-28 19:31:21 +0000

    Refactor mnemonic kit tests slightly.

--- MnemonicKitTests/MnemonicKitTests.swift
+++ MnemonicKitTests/MnemonicKitTests.swift
@@ -4,30 +4,42 @@ import MnemonicKit
 class MnemonicTests: XCTestCase {
 
   func testCreateMnemonic() {
-    let testBundle = Bundle(for: type(of: self))
-    guard let url = testBundle.url(forResource: "vectors", withExtension: "json") else {
-      XCTFail("Unable to find test input file.")
+    guard let vectors = MnemonicTests.dictionaryFromTestInputFile() else {
+      XCTFail("Failed to parse test input file.")
       return
+    }
+
+    if let cases: Array<Array<String>> = vectors["english"] as? Array<Array<String>> {
+      for test in cases {
+        // TODO: Don't use a throwing API here.
+        // TODO: Change errors to english
+        // TODO: Use named constants for test array rather than magic numbers
+        let selfM = try! Mnemonic.mnemonicString(from: test[0], language: .english)
+        let m = test[1]
+        XCTAssertTrue(selfM == m, "计算出的助记词没有通过测试")
+
+        let selfSeed = try! Mnemonic.deterministicSeedString(from: selfM, passphrase: "TREZOR", language: .english)
+        let seed = test[2]
+        XCTAssertTrue(selfSeed == seed, "计算出的 seed 没有通过测试")
+      }
+    }
+  }
+
+  private static func dictionaryFromTestInputFile() ->  [String: Any]? {
+    let testBundle = Bundle(for: self)
+    guard let url = testBundle.url(forResource: "vectors", withExtension: "json") else {
+      return nil
     }
 
     do {
       let data = try Data(contentsOf: url)
-      let vectors: [String: Any] = try JSONSerialization.jsonObject(with: data, options: [.allowFragments, .mutableContainers, .mutableLeaves]) as! [String: Any]
+      let parsedDictionary =
+          try JSONSerialization.jsonObject(with: data,
+                                           options: [.allowFragments, .mutableContainers, .mutableLeaves]) as! [String: Any]
 
-      if let cases: Array<Array<String>> = vectors["english"] as? Array<Array<String>> {
-        for test in cases {
-          let selfM = try Mnemonic.mnemonicString(from: test[0], language: .english)
-          let m = test[1]
-          XCTAssertTrue(selfM == m, "计算出的助记词没有通过测试")
-
-          let selfSeed = try Mnemonic.deterministicSeedString(from: selfM, passphrase: "TREZOR", language: .english)
-          let seed = test[2]
-          XCTAssertTrue(selfSeed == seed, "计算出的 seed 没有通过测试")
-        }
-      }
+      return parsedDictionary
     } catch {
-      XCTFail("Caught an unexpected exception: \(error)")
+      return nil
     }
-
   }
 }
