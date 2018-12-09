419fac2cd2c7a6f4227cd12c075b6921c81630a0
keefertaylor <keefer.t@gmail.com>
2018-12-09 20:04:07 +0000

    Get ExampleApp building

--- Sources/App/AppDelegate.swift
+++ Sources/App/AppDelegate.swift
@@ -8,7 +8,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    print("Successfully built and deployed MnemonicKit.")
+    let viewController = ViewController()
+
+    let window = UIWindow()
+    window.rootViewController = viewController
+    self.window = window
+    window.makeKeyAndVisible()
+
+    if let mnemonic = Mnemonic.generateMnemonic(strength: 64) {
+      let alert = UIAlertController(title: "Generated Mnemonic", message: mnemonic, preferredStyle: .alert)
+      alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "OK"), style: .default, handler: nil))
+      viewController.present(alert, animated: true, completion: nil)
+    }
 
     return true
   }
